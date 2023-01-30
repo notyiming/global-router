0dc889ec6900dff81d167015d2ca06bc1185271d
notyiming <55159184+notyiming@users.noreply.github.com>
2023-01-30 23:18:02 +0000

    rename net module and add docstrings

--- components/net.py
+++ components/net.py
@@ -1,8 +1,10 @@
-from typing import List, Tuple
-from Path import Path
+"""Net Module"""
 
+from typing import List, Tuple
+from components.path import Path
 
 class Net:
+    """Net Class"""
     def __init__(self, net_id, net_name, num_of_pins, pins_coords=[], path=None) -> None:
         self.net_id: int = net_id
         self.net_name: str = net_name
