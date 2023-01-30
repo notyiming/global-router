c2967f94a8845b77200b84822b4f2405ae5238ef
notyiming <55159184+notyiming@users.noreply.github.com>
2023-01-30 23:19:31 +0000

    rename global router module and move into api dir

--- api/global_router.py
+++ api/global_router.py
@@ -1,9 +1,10 @@
+"""Global Router Module"""
+
 import os
-from Net import Net
-from Path import Path
-import utils
-from typing import List, Tuple
 import math
+from typing import List
+from components.net import Net
+from utils import utils
 
 
 class GlobalRouter:
@@ -18,7 +19,7 @@ class GlobalRouter:
         self.netlist: List[Net] = []
         self.total_overflow: int = math.inf
         self.total_wire_length: int = math.inf
-        self.number_of_nodes: int  = 0
+        self.number_of_nodes: int = 0
         self.number_of_edges: int = 0
         self.number_of_horizontal_edges: int = 0
         self.demand: List[float] = []
@@ -40,7 +41,7 @@ class GlobalRouter:
         if os.path.exists(output_file_path):
             file_mode = "w"
 
-        with open(output_file_path, file_mode) as output:
+        with open(output_file_path, file_mode, encoding="UTF-8") as output:
             for net in self.netlist:
                 output.write(f"{net.net_name} {net.net_id}\n")
                 # path = Path()
@@ -48,10 +49,10 @@ class GlobalRouter:
                 output.write("!\n")
 
     def rip_up_and_reroute(self):
-        ...
+        """rip up and reroute"""
 
     def global_route(self):
-        ...
+        """main global routing logic"""
 
     def generate_congestion_output(self, output_file_name: str) -> None:
         """Generate the congestion data for the output
@@ -59,11 +60,12 @@ class GlobalRouter:
         Args:
             output_file_name (str): name of the output file
         """
-        with open(f"{output_file_name}.fig", "x") as output:
+        with open(f"{output_file_name}.fig", "x", encoding="utf-8") as output:
             output.write(f"{self.grid_horizontal_size} {self.grid_vertical_size}\n")
             for i in range(self.number_of_edges):
-                output.write(f"{self.demand[i]/(self.horizontal_capacity if i < self.number_of_horizontal_edges else self.vertical_capacity)} ")
-
+                output.write(
+                    f"{self.demand[i]/(self.horizontal_capacity if i < self.number_of_horizontal_edges else self.vertical_capacity)} "
+                )
 
     @utils.timeit
     def parse_input(self, input_file_path: str) -> List[Net]:
@@ -80,28 +82,33 @@ class GlobalRouter:
         """
         if not os.path.exists(input_file_path):
             raise FileNotFoundError(f"{input_file_path} does not exist.")
-        with open(input_file_path, "r") as input:
-            grid_data = input.readline().split()
+        with open(input_file_path, "r", encoding="utf-8") as file:
+            grid_data = file.readline().split()
             self.grid_horizontal_size = int(grid_data[1])
             self.grid_vertical_size = int(grid_data[2])
-            self.vertical_capacity = int(input.readline().split()[2])
-            self.horizontal_capacity = int(input.readline().split()[2])
-            self.netlist_size = int(input.readline().split()[2])
+            self.vertical_capacity = int(file.readline().split()[2])
+            self.horizontal_capacity = int(file.readline().split()[2])
+            self.netlist_size = int(file.readline().split()[2])
             for _ in range(self.netlist_size):
-                net_info = input.readline().split()
+                net_info = file.readline().split()
                 net_name = net_info[0]
                 net_id = int(net_info[1])
                 num_of_pins = int(net_info[2])
                 net_pins = []
                 for _ in range(num_of_pins):
-                    net_pin = input.readline().split()
+                    net_pin = file.readline().split()
                     net_pin_x = int(net_pin[0])
                     net_pin_y = int(net_pin[1])
                     net_pins.append((net_pin_x, net_pin_y))
                 net = Net(net_id, net_name, num_of_pins)
                 self.netlist.append(net)
             self.number_of_nodes = self.grid_horizontal_size * self.grid_vertical_size
-            self.number_of_horizontal_edges = (self.grid_horizontal_size - 1) * self.grid_vertical_size
-            self.number_of_edges = self.number_of_horizontal_edges + (self.grid_vertical_size - 1) * self.grid_horizontal_size
-            self.demand = [0]*self.number_of_edges
+            self.number_of_horizontal_edges = (
+                self.grid_horizontal_size - 1
+            ) * self.grid_vertical_size
+            self.number_of_edges = (
+                self.number_of_horizontal_edges
+                + (self.grid_vertical_size - 1) * self.grid_horizontal_size
+            )
+            self.demand = [0] * self.number_of_edges
             return self.netlist
