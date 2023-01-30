69bc692ce3e8056e0356101b64bb6a1c453aa6e6
notyiming <55159184+notyiming@users.noreply.github.com>
2023-01-30 23:18:54 +0000

    set up unittest for global router

--- tests/global_router_test.py
+++ tests/global_router_test.py
@@ -1,12 +1,19 @@
+"""Unittests"""
+#!/usr/bin/env python3
+
 import unittest
-from components.GlobalRouter import GlobalRouter
+
+from api.global_router import GlobalRouter
+
 
 class TestGlobalRouter(unittest.TestCase):
+    """Global Router Test Suite"""
     def test_parse_input(self):
-        ...
+        """test parse input"""
         # GlobalRouter.parse_input()
     
     def test_global_router_instance(self):
+        """test global router class instance"""
         self.assertIsInstance(GlobalRouter(), GlobalRouter)
 
 if __name__ == "__main__":
