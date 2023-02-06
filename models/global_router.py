4b8d075a21ef84bc08219649455f5a387ca18710
yiming <ymtan.2062@gmail.com>
2023-02-06 23:01:45 +0000

    implement update_demand, update_overflow, global_route

--- models/global_router.py
+++ models/global_router.py
@@ -2,7 +2,9 @@
 
 import os
 import math
-from typing import List
+# import random
+# import time
+from typing import List, Tuple
 from models.net import Net
 from models.path import Path
 
@@ -18,11 +20,12 @@ class GlobalRouter:
         self.netlist_size: int = 0
         self.netlist: List[Net] = []
         self.total_overflow: int = math.inf
-        self.total_wire_length: int = math.inf
+        self.total_wirelength: int = math.inf
         self.number_of_nodes: int = 0
         self.number_of_edges: int = 0
         self.number_of_horizontal_edges: int = 0
         self.demand: List[float] = []
+        # self.seed: int = 0
 
     def show_netlist_info(self) -> None:
         """Prints netlist info to the terminal"""
@@ -57,8 +60,79 @@ class GlobalRouter:
     def rip_up_and_reroute(self):
         """rip up and reroute"""
 
+    def route_two_pin_net(self, net: Net):
+        """Route a two-pin net
+
+        Args:
+            net (Net): net
+        """
+
+    def update_demand(self, path: Path, increment: bool):
+        """Update demand level for the path
+
+        Args:
+            path (Path): path
+            increment (bool): demand is incremented
+        """
+        for edge_id in path.edge_id_list:
+            if increment:
+                self.demand[edge_id] += 1
+            else:
+                self.demand[edge_id] -= 1
+
+    def update_overflow(self) -> Tuple[int, int]:
+        """Update overflow info for the layout
+
+        Returns:
+            Tuple[int, int]: (total_overflow, wirelength)
+        """
+        max_overflow = 0
+        total_overflow = 0
+        total_wirelength = 0
+        overflow = 0
+        for i in range(self.number_of_edges):
+            total_wirelength += self.demand[i]
+            overflow = self.demand[i] - self.horizontal_capacity if i < self.number_of_horizontal_edges else self.vertical_capacity
+            if overflow <= 0:
+                continue
+            total_overflow += overflow
+            max_overflow = max(overflow, max_overflow)
+        return (total_overflow, total_wirelength)
+
+
     def global_route(self):
         """main global routing logic"""
+        cur_seed = 0
+        num_init_trials = 10
+        seed_feasible = False
+        init_wirelength = math.inf
+        init_overflow: int = 0
+
+        for _ in range(1 if seed_feasible else num_init_trials):
+            self.netlist.sort(key=lambda x: x.net_id)  # reset to original order
+
+            # random.seed(cur_seed if i == (
+            #     1 if seed_feasible else num_init_trials) - 1 else int(time.time()))
+            # random.shuffle(self.netlist)
+            self.netlist.sort(key=lambda x: x.hpwl) # sort by its hpwl
+
+            for net in self.netlist:
+                if net.num_of_pins() == 2:
+                    self.route_two_pin_net(net)
+                self.update_demand(net.path(), True)
+
+            total_overflow, total_wirelength = self.update_overflow()
+            if total_overflow <= init_overflow:
+                if total_wirelength < init_wirelength or \
+                        total_overflow < init_overflow:
+                    # self.seed = cur_seed
+                    init_overflow = total_overflow
+                    init_wirelength = total_wirelength
+
+                    print(
+                        f"seed {cur_seed}, overflow: {self.total_wirelength}, wirelength: {self.total_wirelength}")
+
+        self.netlist.sort(key=lambda x: x.id) # reset to original order
 
     def generate_congestion_output(self, output_file_name: str) -> None:
         """Generate the congestion data for the output
@@ -106,7 +180,7 @@ class GlobalRouter:
                     net_pin_x = int(net_pin[0])
                     net_pin_y = int(net_pin[1])
                     net_pins.append((net_pin_x, net_pin_y))
-                net = Net(net_id, net_name, num_of_pins)
+                net = Net(net_id, net_name, num_of_pins, net_pins)
                 self.netlist.append(net)
             self.number_of_nodes = self.grid_horizontal_size * self.grid_vertical_size
             self.number_of_horizontal_edges = (
