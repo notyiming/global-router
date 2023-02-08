1cbfd80733576ed515e03367b09c2a3379bcf34b
yiming <ymtan.2062@gmail.com>
2023-02-08 13:17:20 +0000

    change log path

--- gr_logger.py
+++ gr_logger.py
@@ -5,7 +5,7 @@ gr_logger = logging.getLogger("gr")
 
 log_formatter = logging.Formatter("%(asctime)s [%(levelname)s]: %(message)s")
 
-file_handler = logging.FileHandler("logs/gr.log", mode="w")
+file_handler = logging.FileHandler("gr.log", mode="w")
 file_handler.setFormatter(log_formatter)
 
 console_handler = logging.StreamHandler()
