cc877f54dd8a5493ed6cf82174da36e82a0feb57
yiming <ymtan.2062@gmail.com>
2023-02-09 21:53:24 +0000

    update logger to rotating file handler

--- gr_logger.py
+++ gr_logger.py
@@ -1,14 +1,28 @@
 """Custom Logger for Global Router"""
 import logging
+import logging.handlers
+import os
+
+LOG_FILENAME = "gr.log"
+should_roll_over = os.path.isfile(LOG_FILENAME)
 
 gr_logger = logging.getLogger("gr")
 
 log_formatter = logging.Formatter("%(asctime)s [%(levelname)s]: %(message)s")
 
-file_handler = logging.FileHandler("gr.log", mode="w")
+file_handler = logging.handlers.RotatingFileHandler(
+    LOG_FILENAME,
+    mode="w",
+    backupCount=5,
+    delay=True
+)
+if should_roll_over:
+    file_handler.doRollover()
 file_handler.setFormatter(log_formatter)
 
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(log_formatter)
 
-logging.basicConfig(level=logging.INFO, handlers=[file_handler, console_handler])
+gr_logger.setLevel(logging.INFO)
+gr_logger.addHandler(file_handler)
+gr_logger.addHandler(console_handler)
