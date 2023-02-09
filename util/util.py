a8e113884fc38786285b3e1fe75ab516e02099bd
yiming <ymtan.2062@gmail.com>
2023-02-09 21:55:32 +0000

    update util decorators

--- util/util.py
+++ util/util.py
@@ -1,19 +1,38 @@
 """Utils Module"""
 
+from functools import wraps
 from time import time
+from gr_logger import gr_logger
 
 def timeit(func):
     """Decorator to time function run time
 
     Args:
-        func (_type_): function to time
+        func (Callable): function to time
     """
 
+    @wraps(func)
     def wrap_func(*args, **kwargs):
         t_1 = time()
         result = func(*args, **kwargs)
         t_2 = time()
-        print(f"Function {func.__name__!r} executed in {(t_2-t_1):.4f}s")
+        gr_logger.info(f"Function {func.__name__} executed in {(t_2-t_1):.4f}s")
         return result
 
     return wrap_func
+
+def log_func(func):
+    """Decorator to log the function with args
+
+    Args:
+        func (Callable): function to log
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            gr_logger.info(f"Running: {func.__name__}")
+            return func(*args, **kwargs)
+        except Exception as ex:
+            gr_logger.exception(ex)
+    # wrapper.__doc__ = func.__doc__
+    return wrapper
