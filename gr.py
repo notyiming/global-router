ec054c751b4830ebecd4ecb9685658d3782f450f
yiming <ymtan.2062@gmail.com>
2023-02-23 17:46:14 +0000

    update draw congestion plot function

--- gr.py
+++ gr.py
@@ -4,7 +4,6 @@
 import math
 import click
 from matplotlib import patches, pyplot as plt
-from matplotlib.lines import Line2D
 from models.global_router import GlobalRouter
 from web.app import app
 
@@ -60,96 +59,88 @@ def gui(port=5000, debug=False):
 
 
 @gr_cli.command()
-@click.option("-c", "--congestion_data_file_path", help="Congestion Data File Path", required=True)
-@click.option("-d", "--display_plot", is_flag=True, help="Display plot")
-def plot_congestion(congestion_data_file_path: str, display_plot=False):
+@click.option("-c", "--congestion_data_file_path", help="Congestion data file path", required=True)
+@click.option("-p", "--plot_figure_filename", help="Plot figure filename", required=True)
+@click.option("-d", "--display_plot_from_cli", is_flag=True, help="Display plot from CLI")
+def plot_congestion(congestion_data_file_path: str, plot_figure_filename: str, display_plot_from_cli=False):
     """Plot congestion data visualization
     \f
 
     Args:
-        congestion_data_file_path (str): Congestion data
+        congestion_data_file_path (str): Congestion data file path 
+        plot_figure_filename (str): Plot figure filename
+        display_plot (bool, optional): Display plot from CLI. Defaults to False.
     """
-    COLOR = ['white', 'blue', 'cyan', 'green', 'yellow', 'red']
-    fig = plt.figure()
-    ax = fig.add_subplot()
+    fig, ax = plt.subplots(1, 1, figsize=(10, 7.5))
+    fig.set_facecolor("black")
+    fig.tight_layout()
     with open(congestion_data_file_path, "r", encoding="utf-8") as data:
         grid_data = data.readline().split()
         grid_hor = int(grid_data[0])
         grid_ver = int(grid_data[1])
         num_hor = (grid_hor - 1)*grid_ver
         congestion_data = [float(n) for n in data.readline().split()]
-        ax.add_patch(
-            patches.Rectangle(
-                (0, 0),
-                0.6*grid_hor - 0.5 + 2,
-                0.6*grid_ver - 0.5 + 2,
-                alpha=1, facecolor='black', edgecolor='black', linestyle='-', fill=True
-            )
-        )
-        ax.add_patch(
-            patches.Rectangle(
-                (0.6*grid_hor - 0.5 + 2, 0),
-                0.2*grid_ver - 0.5 + 2,
-                0.6*grid_ver - 0.5 + 2,
-                alpha=1, facecolor='black', edgecolor='black', linestyle='-', fill=True
-            )
-        )
+
         for i, congestion_level in enumerate(congestion_data):
             if congestion_level == 0:
-                edge_color = COLOR[0]
+                edge_color = "white"
             elif congestion_level <= 0.25:
-                edge_color = COLOR[1]
+                edge_color = "blue"
             elif congestion_level <= 0.5:
-                edge_color = COLOR[2]
+                edge_color = "cyan"
             elif congestion_level <= 0.75:
-                edge_color = COLOR[3]
+                edge_color = "green"
             elif congestion_level <= 1.0:
-                edge_color = COLOR[4]
+                edge_color = "yellow"
             else:
-                edge_color = COLOR[5]
+                edge_color = "red"
 
             if i < num_hor:
-                x = 1 + 0.14 + int(i % (grid_hor-1))*0.6
-                y = 1 + int(i/(grid_hor-1))*0.6
-                ax.add_patch(
-                    patches.Rectangle(
-                        (x, y),
-                        0.4,
-                        0.1,
-                        facecolor=edge_color,
-                        edgecolor=edge_color,
-                        linestyle='-',
-                    )
-                )
+                x = 0.15 + int(i % (grid_hor-1))*0.6
+                y = int(i/(grid_hor-1))*0.6
+                ax.add_patch(patches.Rectangle(
+                    (x, y),
+                    0.4,
+                    0.1,
+                    edgecolor=edge_color,
+                    facecolor=edge_color,
+                    linestyle='-',
+                ))
             else:
-                x = 1 + int((i-num_hor) % (grid_hor))*0.6
-                y = 1 + 0.14 + int((i-num_hor)/(grid_hor))*0.6
-                ax.add_patch(
-                    patches.Rectangle(
-                        (x, y),
-                        0.1,
-                        0.4,
-                        facecolor=edge_color,
-                        edgecolor=edge_color,
-                        linestyle='-',
-                    )
-                )
-    legend_elements = [Line2D([0], [0], color=COLOR[0], lw=4, label='no use'),
-                       Line2D([0], [0], color=COLOR[1],
-                              lw=4, label='util <= 0.25'),
-                       Line2D([0], [0], color=COLOR[2],
-                              lw=4, label='util <= 0.5'),
-                       Line2D([0], [0], color=COLOR[3],
-                              lw=4, label='util <= 0.75'),
-                       Line2D([0], [0], color=COLOR[4],
-                              lw=4, label='util <= 1.0'),
-                       Line2D([0], [0], color=COLOR[5], lw=4, label='overflow')]
-    plt.legend(handles=legend_elements, loc='lower right', title='Legend')
+                x = int((i-num_hor) % (grid_hor))*0.6
+                y = 0.15 + int((i-num_hor)/(grid_hor))*0.6
+                ax.add_patch(patches.Rectangle(
+                    (x, y),
+                    0.1,
+                    0.4,
+                    edgecolor=edge_color,
+                    facecolor=edge_color,
+                    linestyle='-',
+                ))
+    ax.set_aspect('equal', adjustable='box')
+    ax.set_frame_on(False)
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    legend_elements = [
+        patches.Patch(color="white", label="no use"),
+        patches.Patch(color="blue", label="util <= 0.25"),
+        patches.Patch(color="cyan", label="util <= 0.5"),
+        patches.Patch(color="green", label="util <= 0.75"),
+        patches.Patch(color="yellow", label="util <= 1.0"),
+        patches.Patch(color="red", label="overflow")
+    ]
+    ax.legend(
+        handles=legend_elements,
+        title='Legend',
+        loc='center left',
+        bbox_to_anchor=(1, 0.5),
+        fontsize=8
+    )
 
     ax.autoscale_view()
-    plt.savefig("congestion_plots/ming.png", dpi=300)
+    plt.savefig(f"congestion_plots/{plot_figure_filename}", dpi=300)
 
-    if display_plot:
+    if display_plot_from_cli:
         plt.show()
 
 
