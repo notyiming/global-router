8ab58c8ae64341e124367f57b60f9373d75ba973
yiming <ymtan.2062@gmail.com>
2023-03-16 11:58:59 +0000

    move node id setting and node cost calc to node module

--- models/node.py
+++ models/node.py
@@ -1,8 +1,11 @@
 """Node Module"""
-from typing import Tuple
+import math
+from typing import Tuple, List
+
 
 class Node:
     """Node class represents a pin in a net"""
+
     def __init__(self, prev, coordinates) -> None:
         self.edge_id = -1
         self.node_id = -1
@@ -12,3 +15,32 @@ class Node:
 
     def __lt__(self, other):
         return self.cost < other.cost
+
+    def set_node_id(self, grid_horizontal_size: int):
+        """Set ID of the node based on grid layout
+
+        Args:
+           grid_horizontal_size (int): the horizontal size of the grid
+        """
+        self.node_id = grid_horizontal_size * self.coordinates[1] + self.coordinates[0]
+
+    def calculate_cost(self, grid_info: Tuple[List[float], int, int, int]) -> float:
+        """Get the cost of the edge
+
+        Args:
+            grid_info (Tuple[List[float], int, int, int]): Demand, Grid Horizontal Capacity,
+            Grid Vertical Capacity, Number of Horizontal Edges
+
+        Returns:
+            float: cost
+        """
+        demand = grid_info[0]
+        hor_cap = grid_info[1]
+        ver_cap = grid_info[2]
+        num_of_hor_edges = grid_info[3]
+
+        capacity = hor_cap if self.edge_id < num_of_hor_edges else ver_cap
+        if demand[self.edge_id] < capacity:
+            return 1.0 + (demand[self.edge_id] + 1) / capacity
+        else:
+            return math.inf
