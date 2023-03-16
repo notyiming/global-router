8cde2f2593ce45f126fa7705aa25b694dc19098d
yiming <ymtan.2062@gmail.com>
2023-03-16 00:46:27 +0000

    change to deque

--- models/path.py
+++ models/path.py
@@ -1,6 +1,7 @@
 """Path Module"""
 
-from typing import List, Tuple
+from collections import deque
+from typing import Tuple, Deque
 from models.node import Node
 
 
@@ -8,19 +9,17 @@ class Path:
     """Path class represents the full path of a net"""
 
     def __init__(self, end_node) -> None:
-        self.coordinates_list: List[Tuple[int, int]] = []
+        self.coordinates_list: Deque[Tuple[int, int]] = deque()
         self.end_node: Node = end_node
-        self.edge_id_list: List[int] = []
+        self.edge_id_list: Deque[int] = deque()
         self.cost: float = self.end_node.cost
 
         current_grid = self.end_node
 
         # traverse node to get full path coordinates
         while current_grid:
-            self.coordinates_list.append(current_grid.coordinates)
-            self.edge_id_list.append(current_grid.edge_id)
+            self.coordinates_list.appendleft(current_grid.coordinates)
+            self.edge_id_list.appendleft(current_grid.edge_id)
             current_grid = current_grid.prev
 
-        self.edge_id_list.pop()
-        self.coordinates_list.reverse()
-        self.edge_id_list.reverse()
+        self.edge_id_list.popleft()
